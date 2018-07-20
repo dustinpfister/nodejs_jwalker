edb675f7b816aff17a09b37231cc4908301e59f4
dustinpfister <dustin.pfister@gmail.com>
2018-07-20 16:34:37 -0400

    recursive walking

--- jwalker.js
+++ jwalker.js
@@ -1,4 +1,5 @@
-let fs = require('fs');
+let fs = require('fs'),
+path = require('path');
 
 // read a dir using fs.readdir
 let readDir = function (dir) {
@@ -21,18 +22,70 @@ let readDir = function (dir) {
 
     });
 
-}
+};
+
+let readStats = function (itemPath) {
+
+    return new Promise((resolve, reject) => {
+
+        fs.stat(itemPath, function (e, stats) {
+
+            if (e) {
+
+                reject(e)
+
+            } else {
+
+                resolve(stats);
+
+            }
+
+        });
+
+    });
+
+};
+
+// read dir recursive
+let readDirRecursive = function (dir, curDepth, maxDepth) {
+
+    readDir(dir).then(function (items) {
+
+        items.forEach(function (item) {
+
+            let itemPath = path.join(dir, item),
+            nextDepth = curDepth + 1;
+
+            readStats(itemPath).then(function (stats) {
+
+                // if dir
+                if (stats.isDirectory()) {
+
+                    readDirRecursive(itemPath, nextDepth, maxDepth);
+
+                } else {
+
+                    // if file
+
+                    console.log(curDepth + ' + ' + itemPath);
+
+                }
+
+            });
+
+        })
+
+    })
+
+};
 
 module.exports = function (opt) {
 
     opt = opt || {};
 
-    opt.root = opt.root || process.cwd();
+    opt.root = path.resolve(opt.root || process.cwd());
+    opt.depth = opt.depth || -1;
 
-    readDir(opt.root).then(function (items) {
-
-        console.log(items);
-
-    })
+    readDirRecursive(opt.root, 0, opt.depth);
 
 };
